8ee6b1deb94a0a6607a6072f8fea0f9a30989d14
Hampton Rogers <rogershampton22@gmail.com>
2020-10-27 14:10:05 -0500

    Finished OddOrEven

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,27 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IsOdd();
+        }
+
+        public static bool IsOdd()
+        {
+            int num;
+
+            Console.WriteLine("Enter a number");
+
+            num = int.Parse(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Your number is even");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Your number is odd");
+                return true;
+            }
         }
     }
 }
